129ac3abe757b49d82186e6415f6683285510dcf
Naman <namanb454@gmail.com>
2023-04-30 17:02:32 +0530

    first commit

---
+++ src/Player/audios.js
@@ -0,0 +1,14 @@
+exports.songsdata = [
+    {
+        "title": "Rubicon",
+        "url": "rubicon.mp3"
+    },
+    {
+        "title": "Kikli",
+        "url": "kikli.mp3"
+    },
+    {
+        "title": "Mi-Amor",
+        "url": "miamor.mp3"
+    }
+]
